6cf7ef6472b1b016769208fd6934c9bdb0e9a514
Shomirsaidov <abubakr.shomirsadov2@gmail.com>
2025-05-20 11:36:59 +0500

    inshaallah final

--- src/views/CheckerView.vue
+++ src/views/CheckerView.vue
@@ -4,7 +4,7 @@
       <h1 class="text-lg font-semibold text-gray-800">Ҳуҷҷатгузорӣ</h1>
     </header>
 
-  <div class=" max-w-md mx-auto p-6 mt-1 bg-white  shadow-md space-y-6">
+  <div class=" max-w-md mx-auto p-6 mt-1 bg-white  shadow-md space-y-6 pb-40">
     <!-- Title Input -->
     <div>
       <label class="block text-gray-700 font-semibold mb-2">Номи расмии ҳуҷҷат</label>
